1c330d4c058d22fe42947c1c5bfd2c11f652111f
urosaria <urosaria0720@gmail.com>
2015-07-08 16:39:30 +0900

    갤러리형 게시판까지 작업완료!

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,66 @@
+class PostsController < ApplicationController
+
+  before_action :set_bulletin
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = @bulletin.posts.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = @bulletin.posts.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = @bulletin.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to [@post.bulletin, @post], notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to [@post.bulletin, @post], notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to bulletin_posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_bulletin
+    @bulletin = Bulletin.find(params[:bulletin_id])
+  end
+
+  def set_post
+    @post = @bulletin.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :picture, :picture_cache)
+  end
+end
