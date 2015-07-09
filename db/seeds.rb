daccbec52ea4eed02302ee2d40e0ad5135690c7f
urosaria <urosaria0720@gmail.com>
2015-07-09 15:53:47 +0900

    코멘트까지 완료

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+Bulletin.create! title: '공지사항'
+Bulletin.create! title: '새소식'
+Bulletin.create! title: '가입인사', post_type: 'blog'
+Bulletin.create! title: '갤러리', post_type: 'gallery'
 #   Mayor.create(name: 'Emanuel', city: cities.first)
